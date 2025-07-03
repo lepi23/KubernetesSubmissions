32a58597101744b8b6c0e42b81fdfadae6d7b427
Leevi Saarinen <leevi@pop-os.localdomain>
2025-07-03 17:24:11 +0300

    fixed misconfigured ports, fixed missing packages

--- log_output/index.js
+++ log_output/index.js
@@ -27,7 +27,7 @@ app.get('/status', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
